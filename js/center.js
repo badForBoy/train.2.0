6895150bfb27dfa763e7e40e63b5d81a6d002772
badForBoy <731429927@qq.com>
2017-04-10 11:43:51 +0800

    add register

--- js/center.js
+++ js/center.js
@@ -1,14 +1,23 @@
 ;(function(){
+
+	var $team = $(".tab-nav ul li");
+	var $teamMod = $(".tab-modal");
+
+
+	$team.click(function(event) {
+		var itemNum = $(this).index();
+		$(this).addClass('on').siblings($team).removeClass('on');
+		$teamMod.find(".tab-item").eq(itemNum).removeClass('hide').siblings($teamMod).addClass('hide');
+	});
+
 	var $item = $(".main .content .cursole-list .component .all-list .item");
-	//var $modal = $(".main .content .cursole-list .component .details-item-modal");
 	$item.click(function(event) {
 		var $modal = $(this).find(".details-item-modal");
 		$length = $(this).find(".details-item-modal .modal-list").length;
 		$modal.stop(true);
-		$modal.height() == 0 ? $modal.animate({height: $length*60}, 400):$modal.animate({height: 0}, 400);
+		$modal.height() == 0 ? $modal.animate({height: $length*48+60}, 400):$modal.animate({height: 0}, 400);
 	});
 
-	//console.log($(".details-item-modal .modal-list").length)
 	$(".component .c-item").click(function(event) {
 		$(this).addClass('on').siblings($(".component .c-item")).removeClass('on');
 	});
@@ -16,14 +25,13 @@
 
 
 	var $team = $(".content .component .wrap .c-item");
-	var $teamMod = $(".tab-modal-course");
+	var $courseMod = $(".tab-modal-course");
 
 
 	$team.click(function(event) {
 		var itemNum = $(this).index();
-		//console.log(itemNum);
 		$(this).addClass('on').siblings($team).removeClass('on');
-		$teamMod.find(".course-item").eq(itemNum).removeClass('hide').siblings($teamMod).addClass('hide');
+		$courseMod.find(".course-item").eq(itemNum).removeClass('hide').siblings($courseMod).addClass('hide');
 	});
 
 
@@ -34,7 +42,15 @@
 		$(this).parent().parent().find('.modal').show();
 	});
 	$btn.find('.modal .form-header i').click(function(event) {
-		$(this).parent().parent().parent().parent().hide();
+		$(this).parent().parent().parent().hide();
+	});
+
+	var $assnItem = $(".main .content .assn .assn-table ul li");
+	var $assnMod = $(".main .content .assn .assn-modal .item"); 
+	$assnItem.click(function(e) {
+		var itemNum = $(this).index();
+		$(this).addClass('on').siblings($team).removeClass('on');
+		$assnMod.eq(itemNum).removeClass('hide').siblings($teamMod).addClass('hide');
 	});
 
 }());
